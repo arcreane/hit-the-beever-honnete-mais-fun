132f24eacd5b8bfa79bcac844ea6ecee96e64e23
Samia Ouerfelli <samiaouerfelli@MacBook-Pro-de-Samia.local>
2020-10-22 15:08:07 +0200

    Creation of an array for ranking

--- src/com/company/Score.java
+++ src/com/company/Score.java
@@ -1,2 +1,33 @@
-package com.company;public class Score {
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import java.util.Arrays;
+
+public class Score {
+
+    public static void score() {
+
+
+        // Creation of an array
+
+        String[][] ranking = {
+                {"Name1", "Points1"},
+                {"Name2", "Points2"},
+                {"Name3", "Points3"},
+                {"Name4", "Points4"},
+                {"Name5", "Points5"},
+        };
+
+        // Print the informations in the array
+        for (String[] tab: ranking) {
+            for (String x: tab) {
+
+                System.out.print(x + " ");
+            }
+            System.out.println(" ");
+        }
+
+    }
 }
