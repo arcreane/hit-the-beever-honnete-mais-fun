020965ed0130d938719bab40b884b0921d6c21c6
Yoann <yoann.pariset@hotmail.fr>
2020-10-22 10:06:26 +0200

    create gitignore, Grid class and Menu class

---
+++ src/com/company/Menu.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    public static int menu() {
+        Scanner input = new Scanner(System.in);
+        int selection;
+
+        System.out.println("Choose from these choices");
+        System.out.println("-------------------------\n");
+        System.out.println("1 - Start a new game");
+        System.out.println("2 - View HighScore");
+        System.out.println("3 - Quit");
+
+        selection = input.nextInt();
+        return selection;
+    }
+}
